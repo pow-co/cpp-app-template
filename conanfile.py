7f5cf4407f722b1fa7a079c89ea643fa80b84ba5
Zyler <508282+zylerdj@users.noreply.github.com>
2023-01-20 18:59:32 +0100

    Basic layout for Cpp App Template

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+from conans import ConanFile, CMake
+
+class BoostMinerConan(ConanFile):
+    name = "CppAppTemplate"
+    version = "0.2.4"
+    license = "OpenSource"
+    author = "Powco"
+    url = "https://github.com/ProofOfWorkCompany/cpp-app-template"
+    description = "Best practices with c++"
+    topics = ("bitcoin", "mining", "cpu", "sha256", "proofofwork", "boost")
+    settings = "os", "compiler", "build_type", "arch"   
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+    exports_sources = "src/*"
+    #requires = "gtest/1.12.1"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        self.copy("BoostMiner", dst="bin", keep_path=False)
